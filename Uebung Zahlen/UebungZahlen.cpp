4c049be1833933a13b003b62870abd050d6e02a0
wire0 <wire0@DESKTOP-4QGQKU1>
2021-08-09 19:10:59 +0200

    Projektdateien hinzufügen.

---
+++ Uebung Zahlen/UebungZahlen.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <iomanip>
+using namespace std;
+int main()
+{
+	double zahl1, zahl2, zahl3, summe, summenMittelwert; // Speicherbereich/Variablen festlegen.
+	cout << "Erste Zahl eingeben: " << endl;	// Programmablauf/Logik.
+	cin >> zahl1;
+	cout << "Ihre Erste Zahl: " << zahl1 << endl;
+	cout << "Zweite Zahl eingeben: " << endl;
+	cin >> zahl2;
+	cout << "Ihre Zweite Zahl: " << zahl2 << endl;
+	cout << "Dritte Zahl eingeben: " << endl;
+	cin >> zahl3;
+	cout << "Ihre Dritte Zahl; " << zahl3 << endl;
+	cout << endl;
+	cout << endl;
+	summe = zahl1 + zahl2 + zahl3;  // Berechnung und Ausgaben.
+	cout << "Die Gesamtsumme: " << summe << endl;
+	summenMittelwert = summe / 3;
+	cout << "Der Mittelwert: " << summenMittelwert << endl;
+	cout << setw(2);
+	cout << left, cout << fixed, cout << setprecision(2);
+	cout << endl;
+	int *rzahl;		// Hier wird ein Pointer/Zeiger auf die Intger Variable angelegt.
+	cout << "Der Zeiger der lokalen Variable \"RZahl\" zeigt auf folgende Speicheradresse : " << &rzahl;		//Hier wird die Speicher Adresse der lokalen Variablen "RZahl1" angezeigt.
+}
